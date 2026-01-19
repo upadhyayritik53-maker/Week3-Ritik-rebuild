d261bfb60f09942b73a837476c1d1675b3431689
upadhyayritik53-maker <upadhyayritik53@gmail.com>
2026-01-19 22:47:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function toggleMenu() {
-  document.getElementById("navMenu").classList.toggle("open");
-}
+<div class="menu-btn" onclick="toggleMenu()">☰</div>
+
+<nav id="navMenu">
+  <a href="#">Home</a>
+  <a href="#">Services</a>
+  <a href="#">About</a>
+  <a href="#">Contact</a>
+</nav>
