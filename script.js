a9285394be9207798b14a7e11af7d7303728b326
upadhyayritik53-maker <upadhyayritik53@gmail.com>
2026-01-20 22:10:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,13 @@
   <a href="#">About</a>
   <a href="#">Contact</a>
 </nav>
+const cards = document.querySelectorAll('.card');
+
+window.addEventListener('scroll', () => {
+  cards.forEach(card => {
+    const position = card.getBoundingClientRect().top;
+    if (position < window.innerHeight - 100) {
+      card.style.transform = "scale(1.05)";
+    }
+  });
+});
