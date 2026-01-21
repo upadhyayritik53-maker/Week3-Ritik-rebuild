d11d5f246cf9f1ae10b11709189b472cc25edcb5
upadhyayritik53-maker <upadhyayritik53@gmail.com>
2026-01-21 19:50:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-<div class="menu-btn" onclick="toggleMenu()">☰</div>
-
-<nav id="navMenu">
-  <a href="#">Home</a>
-  <a href="#">Services</a>
-  <a href="#">About</a>
-  <a href="#">Contact</a>
-</nav>
-const cards = document.querySelectorAll('.card');
-
-window.addEventListener('scroll', () => {
-  cards.forEach(card => {
-    const position = card.getBoundingClientRect().top;
-    if (position < window.innerHeight - 100) {
-      card.style.transform = "scale(1.05)";
-    }
-  });
+// Button click interaction
+document.querySelector("button").addEventListener("click", () => {
+  alert("Welcome to Static Internship – Week 3!");
 });
