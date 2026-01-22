cc58f5b90578e18c61a46685e7c67dde2109c50a
upadhyayritik53-maker <upadhyayritik53@gmail.com>
2026-01-22 15:44:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 function toggleMenu() {
-  document.getElementById("navMenu").classList.toggle("open");
+  const nav = document.getElementById("navMenu");
+  nav.style.display = nav.style.display === "flex" ? "none" : "flex";
 }
-
-/* ===== SCROLL REVEAL ===== */
-const reveals = document.querySelectorAll(".reveal");
-
-function revealOnScroll() {
-  reveals.forEach(el => {
-    const windowHeight = window.innerHeight;
-    const elementTop = el.getBoundingClientRect().top;
-    if (elementTop < windowHeight - 100) {
-      el.classList.add("active");
-    }
-  });
-}
-
-window.addEventListener("scroll", revealOnScroll);
-revealOnScroll();
