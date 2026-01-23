8303ab181169bbdda070e2ba2a364be8951a9d98
upadhyayritik53-maker <upadhyayritik53@gmail.com>
2026-01-23 13:31:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// Scroll Reveal Animation
-const reveals = document.querySelectorAll(".reveal");
-
-window.addEventListener("scroll", () => {
-  reveals.forEach(section => {
-    const sectionTop = section.getBoundingClientRect().top;
-    const screenHeight = window.innerHeight;
-
-    if (sectionTop < screenHeight - 100) {
-      section.classList.add("active");
-    }
-  });
-});
+console.log("Footer & Responsiveness Loaded");
